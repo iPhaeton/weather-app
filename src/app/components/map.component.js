fc8e564b74183a2e6be09d2419acf15446410365
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-11-27 19:13:31 +0300

    Depict a map

--- src/app/components/map.component.js
+++ src/app/components/map.component.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {NoSupportError} from "../errors/errors.js";
+
 class Map extends React.Component {
-  render() {
-    return (
-      <div>Map here</div>
-    );
-  }
+
+    render() {
+        this.getLocation((err, location) => {
+            if (err) return console.log(err);
+            this.setState({
+                location: location
+            });
+            console.log(this.state.location);
+            this.showMap();
+        });
+
+        return (
+            <div id="map-canvas">Map here</div>
+        );
+    }
+
+    showMap () {
+        var self = this;
+
+        var googleMapsApi = require("google-maps-api")("AIzaSyBIv5Z7Gmo-glNiiqhTqGfISRr-wTQ3MSE");
+        googleMapsApi().then((googleMaps) => {
+            var mapProperties = {
+                center: new googleMaps.LatLng(self.state.location.coords.latitude, self.state.location.coords.longitude),
+                zoom: 10,
+                mapTypeId: googleMaps.MapTypeId.ROADMAP
+            };
+            var map = new googleMaps.Map(document.getElementById("map-canvas"), mapProperties);
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    getLocation (callback) {
+        if (!navigator.geolocation) {
+            return callback(new NoSupportError("Geolocation"));
+        };
+
+        navigator.geolocation.getCurrentPosition((pos) => {
+            callback(null, pos);
+        }, (err) => {
+            callback(err);
+        }, {
+            timeout: 30000
+        });
+    }
+
 }
 
 export default Map;
---
+++ src/app/errors/errors.js
@@ -0,0 +1,14 @@
+export class NoSupportError extends Error{
+    constructor (message) {
+        super (message + "is not supported by your browser");
+
+        this.message = message + "is not supported by your browser";
+        this.name = "NoSupportError";
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, NoSupportError);
+        } else {
+            this.stack = (new Error()).stack;
+        };
+    }
+};
