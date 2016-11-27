2e4e2d5a506b5c66d9e3ddf1e69f1e068825b3d4
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-11-27 11:50:22 +0300

    Fonts for bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,12 @@ module.exports = {
             {
                 test: /\.html$/,
                 loader: "html"
-            }
+            },
+            //for bootstrap
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?name=limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
         ]
     },
 
