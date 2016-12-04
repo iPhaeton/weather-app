5b624a43d5bc7996578dec26d199800d2182cc57
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-12-04 16:58:36 +0300

    Move map functionality into app component.
    1. Store location in a property, not in state, to prevent unnecessary rerendering.

--- src/app/app.js
+++ src/app/app.js
@@ -6,14 +6,51 @@ import Map from './components/map.component';
 
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-          <WeatherDashboard/>
-          <Map/>
-      </div>
-    );
-  }
+    render() {
+        this.getLocation((err, location) => {
+            if (err) return console.log(err);
+            this.location = location;
+            console.log(this.location);
+            this.showMap();
+        });
+
+        return (
+            <div>
+                <WeatherDashboard/>
+                <Map/>
+            </div>
+        );
+    }
+
+    showMap () {
+        var self = this;
+
+        var googleMapsApi = require("google-maps-api")("AIzaSyBIv5Z7Gmo-glNiiqhTqGfISRr-wTQ3MSE");
+        googleMapsApi().then((googleMaps) => {
+            var mapProperties = {
+                center: new googleMaps.LatLng(self.location.coords.latitude, self.location.coords.longitude),
+                zoom: 10,
+                mapTypeId: googleMaps.MapTypeId.ROADMAP
+            };
+            var map = new googleMaps.Map(document.getElementById("map-canvas"), mapProperties);
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    getLocation (callback) {
+        if (!navigator.geolocation) {
+            return callback(new NoSupportError("Geolocation"));
+        };
+
+        navigator.geolocation.getCurrentPosition((pos) => {
+            callback(null, pos);
+        }, (err) => {
+            callback(err);
+        }, {
+            timeout: 30000
+        });
+    }
 }
 
 ReactDOM.render(<App/>, document.getElementById('react-container'));
--- src/app/components/map.component.js
+++ src/app/components/map.component.js
@@ -6,48 +6,9 @@ import {NoSupportError} from "../errors/errors.js";
 class Map extends React.Component {
 
     render() {
-        this.getLocation((err, location) => {
-            if (err) return console.log(err);
-            this.setState({
-                location: location
-            });
-            console.log(this.state.location);
-            this.showMap();
-        });
-
         return (
             <div id="map-canvas">Map here</div>
         );
-    }
-
-    showMap () {
-        var self = this;
-
-        var googleMapsApi = require("google-maps-api")("AIzaSyBIv5Z7Gmo-glNiiqhTqGfISRr-wTQ3MSE");
-        googleMapsApi().then((googleMaps) => {
-            var mapProperties = {
-                center: new googleMaps.LatLng(self.state.location.coords.latitude, self.state.location.coords.longitude),
-                zoom: 10,
-                mapTypeId: googleMaps.MapTypeId.ROADMAP
-            };
-            var map = new googleMaps.Map(document.getElementById("map-canvas"), mapProperties);
-        }, (err) => {
-            console.log(err);
-        });
-    }
-
-    getLocation (callback) {
-        if (!navigator.geolocation) {
-            return callback(new NoSupportError("Geolocation"));
-        };
-
-        navigator.geolocation.getCurrentPosition((pos) => {
-            callback(null, pos);
-        }, (err) => {
-            callback(err);
-        }, {
-            timeout: 30000
-        });
     }
 
 }
