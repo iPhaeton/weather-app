1567232c1c5e9cc225a22fa3c31d31736eccc2c2
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-12-17 16:03:10 +0300

    Move map functionality into map component

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Map from "./components/map.component.js";
 import WeatherDashboard from './components/weather-info.component';
-import Map from './components/map.component';
-import Succession from "./libs/succession";
-import {NoSupportError, ServerResponseError} from "./errors";
+import {NoSupportError} from "./errors";
 
 import mainStyles from "./css/main.css";
 
@@ -13,33 +12,26 @@ class App extends React.Component {
         super();
 
         this.state = {
-            location: {}
+            location: {
+                position: null,
+                place: null
+            },
+            lastChangedProps: new Set()
         };
 
-        var succession = new Succession(
-            [
-                this.getPosition.bind(this),
-                this.getGoogleMaps.bind(this),
-                this.getPlace.bind(this)
-            ], (err, results) => {
-                if (err) console.log(err);
-                else {
-                    this.setLocation({
-                        position: results[0],
-                        place: results[2]
-                    });
-                    console.log(this.state.location);
-                }
+        this.getPosition((err, position) => {
+            if (err) console.log(err);
+            else {
+                this.setLocation({position});
             }
-        );
-        succession.execute();
+        });
     };
 
     render() {
         return (
             <div>
                 <WeatherDashboard/>
-                <Map/>
+                <Map center={this.state.location.position} initiatingStateProps={this.state.lastChangedProps} setLocation={this.setLocation.bind(this)}/>
             </div>
         );
     }
@@ -58,68 +50,16 @@ class App extends React.Component {
         });
     }
 
-    getGoogleMaps (callback, position) {
-        var googleMapsApi = require("google-maps-api")("AIzaSyBIv5Z7Gmo-glNiiqhTqGfISRr-wTQ3MSE");
-
-        googleMapsApi().then((googleMaps) => {
-            this.googleMaps = googleMaps;
-            this.showMap(position);
-            callback(null, googleMaps);
-        }, (err) => {
-            callback(err);
-        });
-    }
-
-    showMap (position) {
-        var coords = position.coords;
-
-        var mapProperties = {
-            center: new this.googleMaps.LatLng(coords.latitude, coords.longitude),
-            zoom: 10,
-            mapTypeId: this.googleMaps.MapTypeId.ROADMAP
-        };
-        var map = new this.googleMaps.Map(document.getElementById(mainStyles.mapCanvas), mapProperties);
-    }
-
-    getPlace (callback, position) {
-        var coords = position.coords;
-
-        var geocoder = new this.googleMaps.Geocoder;
-        geocoder.geocode ({
-            "location": {
-                lat: coords.latitude,
-                lng: coords.longitude
-            }
-        }, (places, status) => {
-            if (status === this.googleMaps.GeocoderStatus.OK) {
-                callback(null, this.getCity(places));
-            }
-            else if (status === this.googleMaps.GeocoderStatus.ZERO_RESULTS) {
-                callback(null, null);
-            }
-            else callback(new ServerResponseError(status, "Google maps Api error"));
-        })
-    }
-
-    getCity (results) {
-        for (var i = results.length-1; i >= 0; i--) {
-            for (var j = 0; j < results[i].types.length; j++) {
-                if (results[i].types[j] === "locality") return results[i];
-            };
-        };
-        return null;
-    }
-
     setLocation (params) {
-        if (params.position === undefined) params.position = this.state.location.position;
-        if (params.place === undefined) params.place = this.state.location.place;
-
         this.setState({
             location: {
-                position: params.position,
-                place: params.place
-            }
+                position: params.position || this.state.location.position,
+                place: params.place || this.state.location.place
+            },
+            lastChangedProps: new Set(Object.getOwnPropertyNames(params).concat("location"))
         });
+
+        console.log(this.state.location);
     }
 }
 
--- src/app/components/map.component.js
+++ src/app/components/map.component.js
@@ -4,12 +4,101 @@ import ReactDOM from 'react-dom';
 import mainStyles from "../css/main.css";
 import bootstrapStyles from "../../vendor/css/bootstrap.min.css";
 
+import Succession from "../libs/succession";
+import {ServerResponseError} from "../errors";
+
 class Map extends React.Component {
 
+    constructor (props) {
+        super(props);
+
+        this.allowRender = true;
+    }
+
     render() {
+        if (!this.props.center) {
+            return (
+                <div>Loading...</div>
+            );
+        };
+
+        if (this.props.initiatingStateProps.has("position")) {
+            var succession = new Succession(
+                [
+                    this.getGoogleMaps.bind(this),
+                    this.getPlace.bind(this)
+                ], (err, results) => {
+                    if (err) console.log(err);
+                    else {
+                        this.allowRender = false;
+                        this.props.setLocation({
+                            place: results[1]
+                        });
+                    }
+                }
+            );
+            succession.execute();
+        };
+
         return (
             <div id={mainStyles.mapCanvas} className={bootstrapStyles["col-md-8"]}>Map here</div>
         );
+    }
+
+    getGoogleMaps (callback) {
+        var googleMapsApi = require("google-maps-api")("AIzaSyBIv5Z7Gmo-glNiiqhTqGfISRr-wTQ3MSE");
+
+        googleMapsApi().then((googleMaps) => {
+            this.googleMaps = googleMaps;
+            try {
+                this.showMap();
+            } catch (err) {
+                callback(err);
+            };
+            callback(null, googleMaps);
+        }, (err) => {
+            callback(err);
+        });
+    }
+
+    showMap () {
+        var coords = this.props.center.coords;
+
+        var mapProperties = {
+            center: new this.googleMaps.LatLng(coords.latitude, coords.longitude),
+            zoom: 10,
+            mapTypeId: this.googleMaps.MapTypeId.ROADMAP
+        };
+        var map = new this.googleMaps.Map(document.getElementById(mainStyles.mapCanvas), mapProperties);
+    }
+
+    getPlace (callback) {
+        var coords = this.props.center.coords;
+
+        var geocoder = new this.googleMaps.Geocoder;
+        geocoder.geocode ({
+            "location": {
+                lat: coords.latitude,
+                lng: coords.longitude
+            }
+        }, (places, status) => {
+            if (status === this.googleMaps.GeocoderStatus.OK) {
+                callback(null, this.getCity(places));
+            }
+            else if (status === this.googleMaps.GeocoderStatus.ZERO_RESULTS) {
+                callback(null, null);
+            }
+            else callback(new ServerResponseError(status, "Google maps Api error"));
+        })
+    }
+
+    getCity (results) {
+        for (var i = results.length-1; i >= 0; i--) {
+            for (var j = 0; j < results[i].types.length; j++) {
+                if (results[i].types[j] === "locality") return results[i];
+            };
+        };
+        return null;
     }
 
 }
